19e3620972a8a58b160be78348078c20d6c3bb9c
Bohdan Scherbak <bogmira777@gmail.com>
2022-07-10 23:06:38 +0200

    Add project files.

---
+++ RoDo/RoDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RoDo
+{
+    public partial class RoDo : Form
+    {
+        public RoDo()
+        {
+            InitializeComponent();
+        }
+    }
+}
